e38a87b93b4a594198ebca1bd629b2ae93a5ea1c
Caulicons <caulicons@hotmail.com>
2022-06-07 15:19:35 -0300

    fist commit - send github

---
+++ README.md
@@ -0,0 +1,7 @@
+# Verificar de Idade 
+
+Aqui você após adicionar uma sua data de nascimento valida - você provavelmente não vai ter mais que 100 anos -, e escolhe seu sexo,  irar retorna sua idade e uma foto de uma pessoa nessa faixa etária. 
+
+Fiz essa página enquanto estava aprendendo o básico do javascript.
+
+Meu tutor foi o grande Guanabara do canal Curso em Vídeo na sua playlist de vídeos de javascript básico patrocinado pelo google. 
---
+++ dinamismo.js
@@ -0,0 +1,62 @@
+function verificar() {
+    let data = new Date()
+    let anoAtual = data.getFullYear()
+    let fano = document.getElementById('textAno')
+    let res = document.getElementById('Resposta')
+
+    if (fano.value < 1930 || fano.value > anoAtual){
+        window.alert('[ERRO] Verifique para ver ser está tudo correto.')
+    } else {
+        let fsex = document.getElementsByName('sexo')
+        let idade = anoAtual - Number(fano.value)  
+        let gênero = ''
+        let img = document.createElement('img')
+        img.setAttribute('id', 'foto') 
+         if (fsex[0].checked) {
+             gênero = 'Homem'
+             if (idade > 0 && idade <= 10){
+                 //criação 
+                 img.setAttribute('src', './imgs/criancaMas.jpg')
+             }
+             else if (idade > 10 && idade < 21) {
+                 //adolescente 
+                 img.setAttribute('src', './imgs/adolescenteMas.jpg')
+             }
+             else if (idade >= 21 && idade < 50) {
+                 //Adulto 
+                 img.setAttribute('src', './imgs/adultoMas.jpg')
+             } 
+             else {
+                 //velho
+                 img.setAttribute('src', './imgs/velhoMas.jpg')
+             }
+         } else if (fsex[1].checked){
+                gênero = 'Feminino'
+                if (idade > 0 && idade <= 10){
+                    //criação 
+                    img.setAttribute('src', './imgs/criancaFem.jpg')
+                }
+                else if (idade > 10 && idade < 21) {
+                    //adolescente 
+                    img.setAttribute('src', './imgs/adolescenteFem.jpg')
+                }
+                else if (idade >= 21 && idade < 50) {
+                    //Adulto 
+                    img.setAttribute('src', './imgs/adultoFem.jpg')
+                } 
+                else {
+                    //velho
+                    img.setAttribute('src', './imgs/velhoFem.jpg')
+                }
+         }
+         res.style.textAlign = 'center'
+         res.innerHTML = `Detectamos ${gênero} com ${idade} anos.`
+         res.appendChild(img)
+    }
+        
+        }
+
+        document.addEventListener("keypress", (event) => {
+            if(event.key === 'Enter') 
+            verificar()
+        })
